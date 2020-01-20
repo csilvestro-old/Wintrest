4a88bd84424fc709b120796040523bae8a7b5be7
chasehoff <crhoffman@student.fullsail.edu>
2020-01-20 10:20:43 -0500

    Added buttons and fixed user profile spacing

--- src/pages/UserProfile/UserProfile.js
+++ src/pages/UserProfile/UserProfile.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import DashHeader from '../../components/DashHeader';
 import StinkyScotty from '../../images/scott.png'
+import { IoIosAdd } from 'react-icons/io'
+import { MdEdit } from 'react-icons/md'
+import { FiShare } from 'react-icons/fi'
+import Selection from '../UserProfile/Selection'
 // import UserForm from './UserForm';
 
 
@@ -22,21 +26,23 @@ class UserProfile extends Component{
         return(
             <div style={styles.container}>
                 <DashHeader avatar={StinkyScotty}/>
-                <div>
-                    <button>Add</button>
-                    
-                    <button>Edit</button>
-                    <button>Share</button>
-                </div>
-                <div style={styles.userInfo}>
-                    <div>
-                        <h1>PRW Lecture</h1>
-                        <h2>0 Followers • 11 Following</h2>
+                <div style={styles.data}>
+                    <div style={styles.buttons}>
+                        <button style={styles.IconBack}><IoIosAdd style={styles.icon} /></button>
+                        <button style={styles.IconBack}><MdEdit style={styles.icon} /></button>
+                        <button style={styles.IconBack}><FiShare style={styles.icon} /></button>
                     </div>
-                    <div>
-                        <img style={{borderRadius: '50%'}} src={StinkyScotty} alt='' />
+                    <div style={styles.userContent}>
+                        <div style={styles.tagInfo}>
+                            <h1 style={{ fontSize: '2rem' }}>PRW Lecture</h1>
+                            <h2>0 Followers • 11 Following</h2>
+                        </div>
+                        <div style={styles.profilePic}>
+                            <img style={{borderRadius: '50%', marginLeft: 'auto', height: '100px'}} src={StinkyScotty} alt='' />
+                        </div>
                     </div>
                 </div>
+                <Selection name={['One', 'Two', 'Three', 'Four']}/>
             </div>
         )
     }
@@ -50,10 +56,35 @@ const styles ={
         flexDirection:'column',
         height:'100vh',
     },
-    userInfo: {
-        width: '80%',
-        margin: '0 auto',
-        backgroundColor: 'lightGray',
-        display: 'flex'
+    data: {
+        width: '50%',
+        margin: '2rem auto 0 auto',
+    },
+    buttons: {
+        display: 'flex',
+        flexDirection: 'row',
+        width: '100%'
+    },
+    tagInfo: {
+        display: 'flex',
+        flexDirection: 'column',
+        margin: 'auto 0',
+        width: '40%'
+    },
+    profilePic: {
+        display: 'flex',
+        flexDirection: 'row',
+        width: '60%'
+    },
+    userContent: {
+        display: 'flex',
+    },
+    IconBack: {
+        backgroundColor: 'transparent'
+    },
+    icon: {
+        height: '30px',
+        width: 'auto',
+        color: 'lightgray'
     }
 }
