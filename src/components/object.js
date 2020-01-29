2ddcfafe6b41d87ee562b61575213841b9cb87d6
Silvestro <csilvestro@csilvestrombp.fullsail.com>
2020-01-29 09:00:46 -0500

    Added object.

---
+++ src/components/object.js
@@ -0,0 +1,121 @@
+{
+    id: '1 - Jerry',
+    name: {
+      first: 'Jerry',
+      last: 'Springer',
+      phone: {
+        cell: '4079334420',
+        work: '4073903994',
+        fax: '40789403945'
+      },
+      address: {
+        stNum: '1b',
+        street: 'NeverLand',
+        state: 'Fl',
+        zip: '33333'
+      }, 
+      nesting: {
+        nesting1: 'some text',
+        nesting2: {
+          some: 'More nesting',
+          some2: 'Even more nesting',
+          some3: {
+            tooMuch: 'Way to much nesting',
+            tooMuch2: 'This is over.'
+          }
+        },
+        nesting3: 'Done'
+      }    
+    }
+  },
+  {
+    id: '2 - Susie',
+    name: {  
+    first: 'Susie',
+    last: 'Bacon',
+    phone: {
+      cell: '4079334420',
+      work: '4073903994',
+      fax: '40789403945'
+    },
+    address: {
+      stNum: '1b',
+      street: 'NeverLand',
+      state: 'Fl',
+      zip: '33333'
+    }, 
+    nesting: {
+      nesting1: 'some text',
+      nesting2: {
+        some: 'More nesting',
+        some2: 'Even more nesting',
+        some3: {
+          tooMuch: 'Way to much nesting',
+          tooMuch2: 'This is over.'
+        }
+      },
+      nesting3: 'Done'
+    }    
+  }
+}, 
+  {
+    id: '3 - Sarah',
+    name: {
+    first: 'Sarah',
+    last: 'Lee',
+    phone: {
+      cell: '4079334420',
+      work: '4073903994',
+      fax: '40789403945'
+    },
+    address: {
+      stNum: '1b',
+      street: 'NeverLand',
+      state: 'Fl',
+      zip: '33333'
+    }, 
+    nesting: {
+      nesting1: 'some text',
+      nesting2: {
+        some: 'More nesting',
+        some2: 'Even more nesting',
+        some3: {
+          tooMuch: 'Way to much nesting',
+          tooMuch2: 'This is over.'
+        }
+      },
+      nesting3: 'Done'
+    }    
+  }
+},    
+  {
+    id: '4 - Ginger',
+    name: {
+    first: 'Ginger',
+    last: 'Cookie',
+    phone: {
+      cell: '4079334420',
+      work: '4073903994',
+      fax: '40789403945'
+    },
+    address: {
+      stNum: '1b',
+      street: 'NeverLand',
+      state: 'Fl',
+      zip: '33333'
+    }, 
+    nesting: {
+      nesting1: 'some text',
+      nesting2: {
+        some: 'More nesting',
+        some2: 'Even more nesting',
+        some3: {
+          tooMuch: 'Way to much nesting',
+          tooMuch2: 'This is over.'
+        }
+      },
+      nesting3: 'Done'
+    }    
+  }
+}
+];
